196bd9cef37f659e692ba4d2c98b7d7fadd9ce05
Sujal Bhandari <116736283+Suj4LB@users.noreply.github.com>
2024-06-01 00:22:09 +0530

    Update reverse_an_array.cpp

--- daily_codes/reverse_an_array.cpp
+++ daily_codes/reverse_an_array.cpp
@@ -1,1 +1,27 @@
+//This is a code to reverse an array using recursion
 
+#include<bits/stdc++.h>
+using namespace std;
+
+void reverseArray(int *l,int *r){
+    if(l>=r) return;
+    int temp = *l;
+    *l = *r;
+    *r = temp;
+    reverseArray(l+1,r-1);
+}
+
+int main(void){
+    int n;cin>>n;
+    int arr[n];
+    for(int i=0;i<n;++i){
+        cin>>arr[i];
+    }
+    int *first = &arr[0];
+    int *last = &arr[n-1];
+    reverseArray(first,last);
+    for(int i=0;i<n;++i){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
